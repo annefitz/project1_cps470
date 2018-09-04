625207e53c8fea9e9414e6d8e6cb4091d6f0c02a
spencer-danielson <42949240+spencer-danielson@users.noreply.github.com>
2018-09-03 21:30:39 -0400

    Update main.cpp

--- project1_cps470/main.cpp
+++ project1_cps470/main.cpp
@@ -26,9 +26,9 @@ int main(int argc, char* argv[])
 	string path = parser.getPath();
 	short port = parser.getPort();
 
-<<<<<<< HEAD
+
 	ws.createTCPSocket();
-=======
+
 	// the following shows how to use winsock functions
 
 	//string host = "www.yahoo.com";
@@ -39,15 +39,13 @@ int main(int argc, char* argv[])
 	// receive reply
 
 	ws.closeSocket(); 
->>>>>>> b9f0ece634e58b1719cde3b8fba3e2cf12e2a0f0
 
 	if (ws.connectToServer(host, port) != 0) {
 
-<<<<<<< HEAD
 	}
 	// construct a GET or HEAD request (in a string), send request
 	if (ws.sendRequest(host, path)) {
-=======
+
 	//string hostIP = "131.238.72.77";  // udayton.edu's IP
 	//ws.createTCPSocket();
 	//ws.connectToServerIP(hostIP, port);
@@ -57,7 +55,6 @@ int main(int argc, char* argv[])
 
 
 	printf("-----------------\n");
->>>>>>> b9f0ece634e58b1719cde3b8fba3e2cf12e2a0f0
 
 	}
 	// receive reply
